ea0dc1948f10a85a52bb3d18861185979585513d
git <git@yoctu.com>
2020-01-17 16:23:28 -0500

    improve input/output and provide sample

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # node-red-contrib-binpacking
+
+node-red-contrib-binpacking is based on the binpackingjs (https://github.com/olragon/binpackingjs) library.
+it only supports 3D pakcing and is primarly designed for logistics purpose.
+
+## Input / Output
+
+### There is 2 inputs :
+
+- A vehicle list called "bins". You must provide a msg.payload.bins as a json array.
+- A parcel or package list you want to transport. You must provide a msg.payload.packages a a json array
+
+### There is 1 output :
+
+- The list of vehicle type with a success true or false ( { "success" : true}). true means the package can fit in the vehicle and false it means it can not. The result is available in the msg.payload.binpacking and the vehicle list order is kept. For each vehicle type the palette also provide which package can fit if they can all be in the vehicle.
+
+## Test
+
+You can check the flow in the sample directory to have a better understanding of the input and output.
+It provides 2 examples :
+- timestamp with predefined set of data
+- api get/post to input values
+
+
+## Contributors and Thanks
+
+Thank you
+@olragon for the binpackingjs library
+@fanshan for the feedback on the palette
+@misterbh for the first version
+@zainiro for using it
+
+LaV.
--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 module.exports = function(RED) {
+  function BinPackingError(node, msg) {
+    node.status({
+      fill: "red",
+      shape: "ring",
+      text: msg
+    });
+    node.error(msg)
+  }
+
   function BinPackingNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
     node.on('input', function(msg) {
-      const result = [];
       const {
         Item,
         Bin,
         Packer
       } = require('binpackingjs').BP3D;
-      const bins = [
-        new Bin("break", 100, 100, 120, 400),
-        new Bin("frg1", 120, 120, 240, 800),
-        new Bin("frg2", 120, 150, 320, 1200),
-        new Bin("frg3", 120, 150, 420, 1200),
-        new Bin("frg4", 200, 200, 200, 1000),
-        new Bin("pl5", 240, 270, 600, 3000),
-        new Bin("pl9", 240, 270, 750, 7500),
-        new Bin("semi", 240, 270, 1360, 24000)
-      ];
-      msg.vehicle = [];
-      let machins = [];
-      msg.payload.forEach((row) => {
-        let k = 0;
-        for (var j = 0; j < row.quantity; j++) {
-          machins.push(new Item("Item " + k, row.width, row.height, row.length, row.weight));
-          k++;
+      msg.payload = [];
+
+      // Bins Setup
+      let bins = [];
+      if (msg.bins && msg.bins.length > 0) {
+        for (const bin in msg.bins) {
+          if (msg.bins[bin].width && msg.bins[bin].height && msg.bins[bin].length && msg.bins[bin].weight) {
+            if (!msg.bins[bin].name) msg.bins[bin].name = "vehicle";
+            bins.push(new Bin(msg.bins[bin].name + " " + bin, msg.bins[bin].width, msg.bins[bin].height, msg.bins[bin].length, msg.bins[bin].weight));
+          } else BinPackingError(this, "invalid msg.bins")
         }
-      });
-      let found = false;
-      bins.forEach((bin, index) => {
-        if (!found) {
-          msg.payload = index;
+      } else BinPackingError(this, "missing msg.bins")
+
+      // Packages Setup
+      let packages = [];
+      if (msg.packages && msg.packages.length > 0) {
+        for (const pkg in msg.packages) {
+          if (!msg.packages[pkg].quantity) msg.packages[pkg].quantity = 1;
+          console.log(msg.packages[pkg].width)
+          if (msg.packages[pkg].width && msg.packages[pkg].height && msg.packages[pkg].length && msg.packages[pkg].weight) {
+            for (let q = 0; q < msg.packages[pkg].quantity; q++) {
+              if (!msg.packages[pkg].name) msg.packages[pkg].name = "Item";
+              packages.push(new Item(msg.packages[pkg].name + " " + q, msg.packages[pkg].width, msg.packages[pkg].height, msg.packages[pkg].length, msg.packages[pkg].weight));
+            }
+          } else BinPackingError(this, "invalid msg.packages")
+        }
+      } else BinPackingError(this, "missing msg.packages")
+
+      if ((bins.length > 0) && (packages.length > 0)) {
+        let binpackingsolution = 0;
+        for (let b = 0; b < bins.length; b++) {
           const packer = new Packer();
-          packer.addBin(bin);
-          packer.items.push(...machins);
+          packer.addBin(bins[b]);
+          packer.items.push(...packages);
+          //packer.items.push(msg.payload);
           packer.pack();
+          msg.payload[b] = {}
+          msg.payload[b].bin = bins[b]
+          msg.payload[b].success = false;
           if (packer.unfitItems.length === 0) {
-            found = true;
-            if (index > 0) {
-              msg.vehicle[0] = bins[index - 1];
-            }
-            msg.vehicle[1] = bins[index];
-            if (index <= bins.length) {
-              msg.vehicle[2] = bins[index + 1];
-            }
+            msg.payload[b].success = true;
+            binpackingsolution++;
           }
         }
-      });
+        if (binpackingsolution === 0) this.status({
+          fill: "yellow",
+          shape: "ring",
+          text: "no solution"
+        });
+        else this.status({
+          fill: "green",
+          shape: "ring",
+          text: binpackingsolution + " solution(s)"
+        });
+      }
       node.send(msg);
     });
   }
